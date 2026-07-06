ab1c2593787d5871db93cc4def09e7367de3cce5
hari charan <kharicharan456@gmail.com>
2026-07-06 19:37:53 +0530

    made changes in load of NN_model

--- Main.py
+++ Main.py
@@ -2,8 +2,13 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import ObModel as om
+import os
 
-NN_model = tf.keras.models.load_model("obesity_model.keras")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+model_path = os.path.join(BASE_DIR, "obesity_model.keras")
+
+NN_model = tf.keras.models.load_model(model_path)
 
 def prediction(features):
     return om.predict(features=features ,model=NN_model)
